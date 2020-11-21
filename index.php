db0608283409e54beb26892fc94bf6e0687fbc3c
furkancelik <furkancelik@sabanciuniv.edu>
2020-11-21 19:19:53 +0300

    updated p and h1 colors

--- index.php
+++ index.php
@@ -5,66 +5,66 @@
 </head>
 <body style="background-color:#424040;">
 
-<h1>Shriekwing</h1>
+<h1 style="color:#FFFFFF;">Shriekwing</h1>
 <p style="color:#FFFFFF;">Shriekwing Phase 1</p>
 <video autoplay loop muted playsinline src="Shriekwing P1.mp4"></video>
 <p style="color:#FFFFFF;">Shriekwing Phase 2</p>
 <video autoplay loop muted playsinline src="Shriekwing P2.mp4"></video>
 
-<h1>Altimor</h1>
-<p>Altimor Phase 1</p>
+<h1 style="color:#FFFFFF;">Altimor</h1>
+<p style="color:#FFFFFF;">Altimor Phase 1</p>
 <video autoplay loop muted playsinline src="Altimor P1.mp4"></video>
-<p>Altimor Phase 2</p>
+<p style="color:#FFFFFF;">Altimor Phase 2</p>
 <video autoplay loop muted playsinline src="Altimor P2.mp4"></video>
-<p>Altimor Phase 3</p>
+<p style="color:#FFFFFF;">Altimor Phase 3</p>
 <video autoplay loop muted playsinline src="Altimor P3.mp4"></video>
 
-<h1>Hungering Destroyer</h1>
-<p>Hungering Destroyer Phase 1</p>
+<h1 style="color:#FFFFFF;">Hungering Destroyer</h1>
+<p style="color:#FFFFFF;">Hungering Destroyer Phase 1</p>
 <video autoplay loop muted playsinline src="Hungering Destroyer P1.mp4"></video>
-<p>Hungering Destroyer Phase 2</p>
+<p style="color:#FFFFFF;">Hungering Destroyer Phase 2</p>
 <video autoplay loop muted playsinline src="Hungering Destroyer P2.mp4"></video>
 
 
-<h1>Artificer Xy'mox</h1>
-<p>Artificer Xy'mox Phase 1</p>
+<h1 style="color:#FFFFFF;">Artificer Xy'mox</h1>
+<p style="color:#FFFFFF;">Artificer Xy'mox Phase 1</p>
 <video autoplay loop muted playsinline src="Artificer Xy'mox P1.mp4"></video>
-<p>Artificer Xy'mox Phase 2</p>
+<p style="color:#FFFFFF;">Artificer Xy'mox Phase 2</p>
 <video autoplay loop muted playsinline src="Artificer Xy'mox P2.mp4"></video>
-<p>Artificer Xy'mox Phase 3</p>
+<p style="color:#FFFFFF;">Artificer Xy'mox Phase 3</p>
 <video autoplay loop muted playsinline src="Artificer Xy'mox P3.mp4"></video>
 
-<h1>Darkvein Container</h1>
-<p>Darkvein Container Phase 1</p>
+<h1 style="color:#FFFFFF;">Darkvein Container</h1>
+<p style="color:#FFFFFF;">Darkvein Container Phase 1</p>
 <video autoplay loop muted playsinline src="Darkvein Container P1.mp4"></video>
-<p>Darkvein Container Phase 2</p>
+<p style="color:#FFFFFF;">Darkvein Container Phase 2</p>
 <video autoplay loop muted playsinline src="Darkvein Container P2.mp4"></video>
-<p>Darkvein Container Phase 3</p>
+<p style="color:#FFFFFF;">Darkvein Container Phase 3</p>
 <video autoplay loop muted playsinline src="Darkvein Container P3.mp4"></video>
-<p>Darkvein Container Phase 3</p>
+<p style="color:#FFFFFF;">Darkvein Container Phase 3</p>
 <video autoplay loop muted playsinline src="Darkvein Container P4.mp4"></video>
 
 
-<h1>Council of Blood</h1>
-<p>Council Dance</p>
+<h1 style="color:#FFFFFF;">Council of Blood</h1>
+<p style="color:#FFFFFF;">Council Dance</p>
 <video autoplay loop muted playsinline src="Council Dance.mp4"></video>
-<p>Council Niklaus</p>
+<p style="color:#FFFFFF;">Council Niklaus</p>
 <video autoplay loop muted playsinline src="Council Niklaus.mp4"></video>
-<p>Council Frieda</p>
+<p style="color:#FFFFFF;">Council Frieda</p>
 <video autoplay loop muted playsinline src="Council Frieda.mp4"></video>
-<p>Council Stavros</p>
+<p style="color:#FFFFFF;">Council Stavros</p>
 <video autoplay loop muted playsinline src="Council Stavros.mp4"></video>
 
 
-<h1>Sludgefist</h1>
-<p>Sludgefist Part 1</p>
+<h1 style="color:#FFFFFF;">Sludgefist</h1>
+<p style="color:#FFFFFF;">Sludgefist Part 1</p>
 <video autoplay loop muted playsinline src="Sludgefist Part 1.mp4"></video>
-<p>Sludgefist Part 2</p>
+<p style="color:#FFFFFF;">Sludgefist Part 2</p>
 <video autoplay loop muted playsinline src="Sludgefist Part 2.mp4"></video>
 
 
-<h1>Sire Denathrius</h1>
-<p>Sire Denathrius Part 1</p>
+<h1 style="color:#FFFFFF;">Sire Denathrius</h1>
+<p style="color:#FFFFFF;">Sire Denathrius Part 1</p>
 <video autoplay loop muted playsinline src="Sire P1.mp4"></video>
 
 </body>
